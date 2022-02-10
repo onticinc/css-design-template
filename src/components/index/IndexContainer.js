97e6a1b3072939a8a70193e8d5d72a9a0e46f907
onticinc <onticinc@gmail.com>
2022-02-09 20:21:45 -0700

    set font sizes

--- src/components/index/IndexContainer.js
+++ src/components/index/IndexContainer.js
@@ -1,16 +1,42 @@
 import React from 'react';
 
+
 const IndexContainer = () => {
     return (
         <div>
-            <div class="call-to-action container grid">
-                <h1>Design System</h1>
+            <body className="bg-dark text-white">
+                <div className="container">
+                    <h1>Design System</h1>
+                    
+                    <div className="bg-white text-dark" style={{padding: "1rem"}}>
+                        light blue bg, with dark text
+                    </div>
+                    <div className="bg-accent text-accent" style={{padding: "1rem"}}>
+                        white bg, with dark text
+                    </div>
+                    <div className="bg-white text-white" style={{padding: "1rem"}}>
+                        light blue bg with 20% akoga and white text
+                    </div>
 
-                <section></section>
-            </div> 
+                    <section id="colors">
+                        <h2><span>01</span> Colors</h2>
+                        <div style={{padding: "3rem 1rem 1rem", border: '1px solid white'}}>
+
+                        </div>
+                    </section>
+                    <p className='text-white'>
+                        Elit voluptate consectetur in consectetur fugiat id tempor fugiat est. Officia exercitation sit cillum voluptate mollit Lorem eiusmod. Tempor esse nulla quis pariatur duis anim labore sunt dolor. Deserunt pariatur irure labore aliqua cupidatat cupidatat adipisicing voluptate anim cillum. Nisi ut fugiat laboris quis dolore deserunt.
+                    </p>
+                    <p className="text-white">
+                        Aliquip ex occaecat minim dolore minim eu. Esse officia qui aute labore ut mollit pariatur. Nulla et sit cupidatat occaecat do et quis excepteur aliquip cupidatat. Officia ipsum ipsum Lorem laborum velit consectetur sit. Et adipisicing consectetur consequat non eiusmod cillum aliquip elit aliquip ut id pariatur. Culpa et veniam dolore consequat esse et consectetur fugiat cupidatat consectetur proident id ad.
+                    </p>
+
+
+                </div> 
+            </body>
         </div>
     )
-}
+} 
 
 export default IndexContainer;
 
